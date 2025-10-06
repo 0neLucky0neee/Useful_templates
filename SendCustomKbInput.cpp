5317ae30d41de56f7f13986d5518abbc1a34f9b3
0neLucky0neee <0neLucky0neee@gmail.com>
2025-10-06 15:46:04 +0300

    Create SendCustomKbInput.cpp

---
+++ SendCustomKbInput.cpp
@@ -0,0 +1,9 @@
+// learn.microsoft.com/en-us/windows/win32/inputdev/about-keyboard-input
+void SendCustomKbInput(WORD wScan, DWORD dwUp) {
+	INPUT iInputs[0x1] = { NULL };
+	iInputs[0x0].type = INPUT_KEYBOARD;
+	iInputs[0x0].ki.dwFlags = (dwUp == TRUE ? KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP : KEYEVENTF_SCANCODE);
+	iInputs[0x0].ki.wScan = wScan;
+
+	SendInput(1, iInputs, sizeof(INPUT));
+}
