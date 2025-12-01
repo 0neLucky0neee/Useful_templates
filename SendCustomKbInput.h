1fd91622a10963a6984a2ab595ed95cdb7ebac95
0neLucky0neee <0neLucky0neee@gmail.com>
2025-12-01 22:40:59 +0200

    Update SendCustomKbInput.h

--- SendCustomKbInput.h
+++ SendCustomKbInput.h
@@ -1,6 +1,15 @@
 #pragma once
 #include <Windows.h>
 
+#define SCAN_KEY_W		0x0011
+#define SCAN_KEY_A		0x001E
+#define SCAN_KEY_S		0x001F
+#define SCAN_KEY_D		0x0020
+
+#define SCAN_KEY_F		0x0021
+
+#define SCAN_KEY_SPACE	0x0039
+
 // learn.microsoft.com/en-us/windows/win32/inputdev/about-keyboard-input
 void SendCustomKbInput(WORD wScan, DWORD dwUp) {
 	INPUT iInputs[0x1] = { NULL };
