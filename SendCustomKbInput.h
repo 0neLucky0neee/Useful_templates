0c7290ca7b7eeddc916bfa37503f074b0a1b47df
0neLucky0neee <0neLucky0neee@gmail.com>
2025-12-01 22:53:19 +0200

    Update SendCustomKbInput.h

--- SendCustomKbInput.h
+++ SendCustomKbInput.h
@@ -9,6 +9,10 @@
 #define SCAN_KEY_F		0x0021
 
 #define SCAN_KEY_SPACE	0x0039
+#define SCAN_KEY_LSHIFT 0x002A
+
+#define SCAN_KEY_LALT	0x0038
+#define SCAN_KEY_TAB	0x000F
 
 // learn.microsoft.com/en-us/windows/win32/inputdev/about-keyboard-input
 void SendCustomKbInput(WORD wScan, DWORD dwUp) {
